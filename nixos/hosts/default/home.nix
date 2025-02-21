3390320111a5d25505b68dfa6055d63b61553dc4
Martin <martinjonsson01@gmail.com>
2025-02-21 17:39:25 +0100

    17 current  2025-02-21 17:39:24  24.11.20250219.36864ed     6.6.78                          *

--- nixos/hosts/default/home.nix
+++ nixos/hosts/default/home.nix
@@ -45,8 +45,6 @@
     (pkgs.writeShellScriptBin "discord" ''
       ${pkgs.discord}/bin/discord \
           --ignore-gpu-blocklist \
-          --disable-features=UseOzonePlatform \
-          --enable-features=VaapiVideoDecoder \
           --use-gl=desktop \
           --enable-gpu-rasterization \
           --enable-zero-copy \
