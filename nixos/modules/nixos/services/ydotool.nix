690578bc910bb51dd7f4ef6e1f5b3f7a08deca5b
Martin <martinjonsson01@gmail.com>
2025-02-22 17:37:58 +0100

    95 current  2025-02-22 17:37:55  24.11.20250219.36864ed     6.6.78                          *

--- nixos/modules/nixos/services/ydotool.nix
+++ nixos/modules/nixos/services/ydotool.nix
@@ -16,47 +16,16 @@
       ydotool
     ];
 
-    # https://github.com/NixOS/nixpkgs/issues/70471
-    # Chown&chmod /dev/uinput to owner:root group:input mode:0660
-    boot.kernelModules = ["uinput"];
-    services.udev.extraRules = ''
-      SUBSYSTEM=="misc", KERNEL=="uinput", TAG+="uaccess", OPTIONS+="static_node=uinput", GROUP="input", MODE="0660"
-    '';
-
-    users.users.libinput = {
-      group = config.users.groups.input.name;
-      description = "libinput user";
-      createHome = false;
-      isSystemUser = true;
-      inherit (config.users.users.nobody) home;
-    };
-
-    systemd.services.ydotoold = lib.mkIf config.ydotool.enable {
-      description = "Ydotoold virtual input device";
-
-      partOf = ["graphical.target"];
-      requires = ["graphical.target"];
-      after = ["graphical.target"];
-      wantedBy = ["graphical.target"];
-
-      serviceConfig = {
-        ExecStartPre = pkgs.writeShellScript "delete-ydotool-socket" ''
-          ${pkgs.coreutils}/bin/rm /tmp/.ydotool_socket || true
-        '';
-        ExecStart = "${pkgs.ydotool}/bin/ydotoold";
-        ExecStopPost = pkgs.writeShellScript "delete-ydotool-socket" ''
-          ${pkgs.coreutils}/bin/rm /tmp/.ydotool_socket || true
-        '';
-        ExecReload = "systemctl kill --signal=HUP $MAINPID";
-        KillMode = "process";
-        TimeoutSec = 100;
-        Restart = "on-failure";
-        User = config.users.users.libinput.name;
-        Group = config.users.users.libinput.group;
-
-        ProtectHome = "read-only";
-        PrivateTmp = false;
+    systemd.services.ydotoold = {
+      enable = true;
+      description = "RIP Mouse, thou shalt be missed";
+      unitConfig = {
+        Type = "simple";
       };
+      serviceConfig = {
+        ExecStart = "${pkgs.ydotool}/bin/ydotoold";
+      };
+      wantedBy = ["multi-user.target"];
     };
   };
 }
