5ce1087847527f9667a8eb76f1492ccf191642ae
Martin <martinjonsson01@gmail.com>
2025-02-22 07:49:41 +0100

    41 current  2025-02-22 07:49:39  24.11.20250219.36864ed     6.6.78                          *

--- nixos/hosts/default/home.nix
+++ nixos/hosts/default/home.nix
@@ -89,6 +89,9 @@
     # EDITOR = "emacs";
   };
 
+  # Nicely reload system units when changing configs
+  systemd.user.startServices = "sd-switch";
+
   # Let Home Manager install and manage itself.
   programs.home-manager.enable = true;
 
