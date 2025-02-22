25d72131035125f17ca7cc20e0d700c6f49f41d2
Martin <martinjonsson01@gmail.com>
2025-02-22 15:43:33 +0100

    66 current  2025-02-22 15:43:31  24.11.20250219.36864ed     6.6.78                          *

--- nixos/modules/home-manager/launchers/rofi.nix
+++ nixos/modules/home-manager/launchers/rofi.nix
@@ -79,92 +79,10 @@ in {
         sorting-method = "fzf";
       };
 
-      # Based on Newman Sánchez's Launchpad theme <https://github.com/newmanls/rofi-themes-collection>
-      theme = mkForce {
-        "*" = {
-          font = "Noto Sans CJK JP Bold 12";
-          background-color = mkLiteral "transparent";
-          foreground = mkLiteral "${base05}";
-          text-color = mkLiteral "${base05}";
-          padding = mkLiteral "0px";
-          margin = mkLiteral "0px";
-        };
-
-        window = {
-          fullscreen = true;
-          padding = mkLiteral "1em";
-          background-color = mkLiteral "${base00}dd";
-        };
-
-        mainbox = {
-          padding = mkLiteral "8px";
-        };
-
-        inputbar = {
-          background-color = mkLiteral "${base05}20";
-
-          margin = mkLiteral "0px calc( 50% - 230px )";
-          padding = mkLiteral "4px 8px";
-          spacing = mkLiteral "8px";
-
-          border = mkLiteral "1px";
-          border-radius = mkLiteral "2px";
-          border-color = mkLiteral "${base05}40";
-
-          children = map mkLiteral [
-            "icon-search"
-            "entry"
-          ];
-        };
-
-        prompt = {
-          enabled = false;
-        };
-
-        icon-search = {
-          expand = false;
-          filename = "search";
-          vertical-align = mkLiteral "0.5";
-        };
-
-        entry = {
-          placeholder = "Search";
-          placeholder-color = mkLiteral "${base05}20";
-        };
-
-        listview = {
-          margin = mkLiteral "48px calc( 50% - 720px )";
-          margin-bottom = mkLiteral "0px";
-          spacing = mkLiteral "48px";
-          columns = 6;
-          fixed-columns = true;
-        };
-
-        "element, element-text, element-icon" = {
-          cursor = mkLiteral "pointer";
-        };
-
-        element = {
-          padding = mkLiteral "8px";
-          spacing = mkLiteral "4px";
-
-          orientation = mkLiteral "vertical";
-          border-radius = mkLiteral "12px";
-        };
-
-        "element selected" = {
-          background-color = mkLiteral "${base05}33";
-        };
-
-        element-icon = {
-          size = mkLiteral "5.75em";
-          horizontal-align = mkLiteral "0.5";
-        };
-
-        element-text = {
-          horizontal-align = mkLiteral "0.5";
-        };
-      };
+      theme = "${pkgs.fetchurl {
+        url = "https://github.com/undiabler/nord-rofi-theme/raw/eebddcbf36052e140a9af7c86f1fbd88e31d2365/nord.rasi";
+        sha256 = "sha256-3P7Fpsev0Y7oBtK+x2R4V4aCkdQThybUSySufNFGtl4=";
+      }}";
     };
   };
 }
