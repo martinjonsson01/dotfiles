f449fe72c4071af038be0d58fdd329b1601a211d
Martin <martinjonsson01@gmail.com>
2025-02-22 18:20:47 +0100

    117 current  2025-02-22 18:20:44  24.11.20250219.36864ed     6.6.78                          *

--- nixos/modules/home-manager/desktops/gnome.nix
+++ nixos/modules/home-manager/desktops/gnome.nix
@@ -59,6 +59,12 @@
         toggle-quick-settings = [];
       };
 
+      "org/gnome/settings-daemon/plugins/color" = {
+        night-light-enabled = true;
+        night-light-schedule-from = 18;
+        night-light-schedule-to = 5;
+      };
+
       # "org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0" = {
       #   binding = "Super_L";
       #   command = "rofi -show combi -calc-command \"echo -n '{result}' | wl-copy\" -calc-command-history";
