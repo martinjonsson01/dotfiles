01c28dfbbb7b06509ab1648b28256bd1121c7dbe
Martin <martinjonsson01@gmail.com>
2025-02-24 19:42:41 +0100

    251 current  2025-02-24 19:42:35  24.11.20250219.36864ed     6.6.78                          *

--- nixos/modules/home-manager/desktops/hyprland/config/appearances.nix
+++ nixos/modules/home-manager/desktops/hyprland/config/appearances.nix
@@ -18,5 +18,6 @@
     new_on_top = true; # whether a newly open window should be on the top of the stack
     mfact = 0.45; # the size as a percentage of the master window, for example mfact = 0.70 would mean 70% of the screen will be the master window, and 30% the slave
     allow_small_split = true; # enable adding additional master windows in a horizontal split style
+    always_center_master = true;
   };
 }
