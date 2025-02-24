974b4e0cac87b32bfc18f74bc5eb075a3e17aa22
Martin <martinjonsson01@gmail.com>
2025-02-24 19:36:21 +0100

    247 current  2025-02-24 19:36:16  24.11.20250219.36864ed     6.6.78                          *

--- nixos/modules/home-manager/desktops/hyprland/config/appearances.nix
+++ nixos/modules/home-manager/desktops/hyprland/config/appearances.nix
@@ -15,6 +15,7 @@
   };
 
   master = {
-    new_on_top = true;
+    new_on_top = true; # whether a newly open window should be on the top of the stack
+    mfact = 0.45; # the size as a percentage of the master window, for example mfact = 0.70 would mean 70% of the screen will be the master window, and 30% the slave
   };
 }
