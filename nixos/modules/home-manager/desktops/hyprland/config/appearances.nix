d80f36cb5a63bf1ec73ea3f6b71c72326ddfa483
Martin <martinjonsson01@gmail.com>
2025-02-26 17:38:51 +0100

    283 current  2025-02-26 17:38:45  24.11.20250219.36864ed     6.6.78                          *

--- nixos/modules/home-manager/desktops/hyprland/config/appearances.nix
+++ nixos/modules/home-manager/desktops/hyprland/config/appearances.nix
@@ -11,5 +11,18 @@
     inactive_opacity = 0.7;
     dim_inactive = true;
     dim_strength = 0.3;
+    blur = {
+      enabled = true;
+      size = 7;
+      passes = 4;
+      noise = 0.008;
+      contrast = 0.8916;
+      brightness = 0.8;
+      input_methods = "yes";
+    };
+
+    shadow = {
+      enabled = false;
+    };
   };
 }
