3013802e70c67a71a11ddcd2f2c864179c855eda
Martin <martinjonsson01@gmail.com>
2025-02-27 17:33:52 +0100

    304 current  2025-02-27 17:33:45  24.11.20250219.36864ed     6.6.78                          *

--- nixos/modules/home-manager/desktops/add-ons/flameshot.nix
+++ nixos/modules/home-manager/desktops/add-ons/flameshot.nix
@@ -3,9 +3,24 @@
 #
 {
   lib,
+  pkgs,
   config,
   ...
-}: {
+}: let
+  flameshotGrim = pkgs.flameshot.overrideAttrs (oldAttrs: {
+    src = pkgs.fetchFromGitHub {
+      owner = "flameshot-org";
+      repo = "flameshot";
+      rev = "3d21e4967b68e9ce80fb2238857aa1bf12c7b905";
+      sha256 = "sha256-OLRtF/yjHDN+sIbgilBZ6sBZ3FO6K533kFC1L2peugc=";
+    };
+    cmakeFlags = [
+      "-DUSE_WAYLAND_CLIPBOARD=1"
+      "-DUSE_WAYLAND_GRIM=1"
+    ];
+    buildInputs = oldAttrs.buildInputs ++ [pkgs.libsForQt5.kguiaddons];
+  });
+in {
   options = {
     flameshot.enable = lib.mkEnableOption "Enables Flameshot";
   };
@@ -13,6 +28,7 @@
   config = lib.mkIf config.flameshot.enable {
     services.flameshot = {
       enable = true;
+      package = flameshotGrim;
       settings = {
         General = {
           uiColor = "#1435c7";
