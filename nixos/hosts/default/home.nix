16a887f18bdbfc6ff7800c625a6bb457aa8b2775
Martin <martinjonsson01@gmail.com>
2025-02-28 16:12:27 +0100

    326 current  2025-02-28 16:12:20  24.11.20250219.36864ed     6.6.78                          *

--- nixos/hosts/default/home.nix
+++ nixos/hosts/default/home.nix
@@ -90,7 +90,7 @@
   # plain files is through 'home.file'.
   home.file = {
     # # Building this configuration will create a copy of 'dotfiles/screenrc' in
-    # # the Nix store. Activating the configuration will then make '~/.screenrc' a
+    # # the Nix store.  Activating the configuration will then make '~/.screenrc' a
     # # symlink to the Nix store copy.
     # ".screenrc".source = dotfiles/screenrc;
 
