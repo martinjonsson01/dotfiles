f9c81b30bbb0f280eefe569fe90467aa26fa2cc9
Martin <martinjonsson01@gmail.com>
2025-02-28 18:13:17 +0100

    336 current  2025-02-28 18:13:10  24.11.20250219.36864ed     6.6.78                          *

--- nixos/modules/home-manager/desktops/gnome.nix
+++ nixos/modules/home-manager/desktops/gnome.nix
@@ -74,6 +74,30 @@
         toggle-quick-settings = [];
       };
 
+      "org/gnome/shell/extensions/advanced-alt-tab-window-switcher" = {
+        app-switcher-popup-fav-apps = false;
+        app-switcher-popup-filter = 3;
+        app-switcher-popup-include-show-apps-icon = false;
+        app-switcher-popup-win-counter = false;
+        enable-super = false;
+        hot-edge-fullscreen = false;
+        hot-edge-mode = 1;
+        hot-edge-position = 0;
+        show-dash = 2;
+        super-double-press-action = 2;
+        super-key-mode = 1;
+        switcher-popup-hover-select = false;
+        switcher-popup-interactive-indicators = false;
+        switcher-popup-monitor = 3;
+        switcher-popup-position = 3;
+        switcher-popup-preview-selected = 3;
+        switcher-popup-start-search = false;
+        switcher-popup-timeout = 0;
+        switcher-popup-up-down-action = 1;
+        win-switch-include-modals = true;
+        win-switch-mark-minimized = false;
+      };
+
       "org/gnome/settings-daemon/plugins/color" = {
         night-light-enabled = true;
         night-light-schedule-from = 19.0;
