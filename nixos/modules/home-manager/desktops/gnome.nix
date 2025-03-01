27adbdec4e6835bea573a4977d43d8b11e70be86
Martin <martinjonsson01@gmail.com>
2025-03-01 18:37:16 +0100

    366 current  2025-03-01 18:37:09  24.11.20250219.36864ed     6.6.78                          *

--- nixos/modules/home-manager/desktops/gnome.nix
+++ nixos/modules/home-manager/desktops/gnome.nix
@@ -116,7 +116,7 @@
 
       "org/gnome/settings-daemon/plugins/color" = {
         night-light-enabled = true;
-        night-light-schedule-from = 19.0;
+        night-light-schedule-from = 19.5;
         night-light-schedule-to = 5.0;
       };
 
