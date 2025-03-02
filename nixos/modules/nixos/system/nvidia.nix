5775dc7b77f68c9f7563b2852c41686d770435d5
Martin <martinjonsson01@gmail.com>
2025-03-02 08:03:04 +0100

    376 current  2025-03-02 08:02:56  24.11.20250219.36864ed     6.6.78                          *

--- nixos/modules/nixos/system/nvidia.nix
+++ nixos/modules/nixos/system/nvidia.nix
@@ -11,6 +11,7 @@
     # Load nvidia driver for Xorg and Wayland
     services.xserver.videoDrivers = ["nvidia"];
 
+    # Disables modeset (?)
     boot.kernelParams = ["nomodeset" "nvidia-drm.modeset=1"];
 
     hardware.nvidia = {
@@ -35,7 +36,7 @@
       # supported GPUs is at:
       # https://github.com/NVIDIA/open-gpu-kernel-modules#compatible-gpus
       # Only available from driver 515.43.04+
-      open = false;
+      open = true;
 
       # Enable the Nvidia settings menu,
       # accessible via `nvidia-settings`.
