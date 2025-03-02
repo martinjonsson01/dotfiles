9707436f0d63c46f1e96ffc83537607cd417db6d
Martin <martinjonsson01@gmail.com>
2025-03-02 13:32:07 +0100

    391 current  2025-03-02 13:31:57  24.11.20250219.36864ed     6.6.78                          *

--- nixos/modules/home-manager/shells/fish/functions/command_not_found.fish
+++ nixos/modules/home-manager/shells/fish/functions/command_not_found.fish
@@ -1,3 +1,3 @@
-function fish_command_not_found --on-event fish_command_not_found
-    ~/bin/command-not-found $argv
-end
+# function fish_command_not_found --on-event fish_command_not_found
+#     ~/bin/command-not-found $argv
+# end
