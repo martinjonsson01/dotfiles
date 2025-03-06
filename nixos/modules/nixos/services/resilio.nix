6a00d0ef302f638c378672dce2df3ef2cfbed54b
Martin <martinjonsson01@gmail.com>
2025-03-06 17:34:25 +0100

    411 current  2025-03-06 17:34:15  24.11.20250219.36864ed     6.6.78                          *

--- nixos/modules/nixos/services/resilio.nix
+++ nixos/modules/nixos/services/resilio.nix
@@ -21,21 +21,21 @@
 
       deviceName = config.networking.hostName;
 
-      enableWebUI = false;
-      sharedFolders = [
-        {
-          directory = "/big-chungus/Drive";
-          secretFile = config.sops.secrets."resilio-secret".path;
-          knownHosts = [
-            "192.168.0.162"
-          ];
-          searchLAN = true;
-          useDHT = false;
-          useRelayServer = true;
-          useSyncTrash = true;
-          useTracker = true;
-        }
-      ];
+      enableWebUI = true;
+      # sharedFolders = [
+      #   {
+      #     directory = "/big-chungus/Drive";
+      #     secretFile = config.sops.secrets."resilio-secret".path;
+      #     knownHosts = [
+      #       "192.168.0.162:28889"
+      #     ];
+      #     searchLAN = true;
+      #     useDHT = false;
+      #     useRelayServer = true;
+      #     useSyncTrash = true;
+      #     useTracker = true;
+      #   }
+      # ];
     };
   };
 }
