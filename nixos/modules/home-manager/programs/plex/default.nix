e52c9640eeecf05bf633a2ebfcf53893fe60f780
Martin <martinjonsson01@gmail.com>
2025-03-07 18:10:46 +0100

    434 current  2025-03-07 18:10:38  24.11.20250219.36864ed     6.6.78                          *

--- nixos/modules/home-manager/programs/plex/default.nix
+++ nixos/modules/home-manager/programs/plex/default.nix
@@ -4,21 +4,21 @@
   config,
   ...
 }: let
-  patchedMpvPlex = pkgs.plex-desktop.overrideAttrs (previousAttrs: {
-    postInstall = ''
-      ${previousAttrs.postInstall or ""}
-
-      # Remove built-in libmvp (save backup of it),
-      # then symlink to updated libmpv
-      ln --backup --force --symbolic --target-directory=${pkgs.plex-desktop}/lib ${pkgs.mpv}/lib/libmvp.so.2
-    '';
-  });
-
-  patch = pkgs.runCommandNoCC "plex-desktop-mpv-patch" {} ''
-    # Remove built-in libmvp (save backup of it),
-    # then symlink to updated libmpv
-    ln --backup --force --symbolic --target-directory=${pkgs.plex-desktop}/lib ${pkgs.mpv}/lib/libmvp.so.2
-  '';
+  # patchedMpvPlex = pkgs.plex-desktop.overrideAttrs (previousAttrs: {
+  #   postInstall = ''
+  #     ${previousAttrs.postInstall or ""}
+  #
+  #     # Remove built-in libmvp (save backup of it),
+  #     # then symlink to updated libmpv
+  #     ln --backup --force --symbolic --target-directory=${pkgs.plex-desktop}/lib ${pkgs.mpv}/lib/libmvp.so.2
+  #   '';
+  # });
+  #
+  # patch = pkgs.runCommandNoCC "plex-desktop-mpv-patch" {} ''
+  #   # Remove built-in libmvp (save backup of it),
+  #   # then symlink to updated libmpv
+  #   ln --backup --force --symbolic --target-directory=${pkgs.plex-desktop}/lib ${pkgs.mpv}/lib/libmvp.so.2
+  # '';
 in {
   options = {
     plex-desktop.enable = lib.mkEnableOption "Enables Plex Desktop";
