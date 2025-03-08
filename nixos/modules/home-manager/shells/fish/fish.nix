29ac0c79d72f491411eeefed46ea73fe87da650d
Martin <martinjonsson01@gmail.com>
2025-03-08 12:37:43 +0100

    446 current  2025-03-08 12:37:34  24.11.20250219.36864ed     6.6.78                          *

--- nixos/modules/home-manager/shells/fish/fish.nix
+++ nixos/modules/home-manager/shells/fish/fish.nix
@@ -55,8 +55,8 @@
       direnv.enable = true;
       direnv.nix-direnv.enable = true;
       # Creates programs.sqlite database for command-not-found.
-      nix-index.enable = true;
-      nix-index.enableFishIntegration = true;
+      # nix-index.enable = true;
+      # nix-index.enableFishIntegration = true;
     };
 
     programs.fish = {
