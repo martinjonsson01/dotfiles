7e50b8f7f8e64bac6b536d86453456d08d4138f8
Martin <martinjonsson01@gmail.com>
2025-03-08 16:36:04 +0100

    461 current  2025-03-08 16:35:56  24.11.20250219.36864ed  6.6.78                          *

--- nixos/modules/home-manager/desktops/gnome/default.nix
+++ nixos/modules/home-manager/desktops/gnome/default.nix
@@ -226,7 +226,7 @@
       };
 
       "org/gnome/shell/extensions/dash-to-panel" = {
-        panel-sizes = "{\"0\": 32,\"1\": 32,\"2\": 48}";
+        panel-sizes = "{\"0\": 64,\"1\": 64,\"2\": 64}";
 
         panel-positions-monitors-sync = false;
         panel-positions = "{\"2\":\"RIGHT\"}";
