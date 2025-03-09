fc8698f51527fc1b661d89c5614f1b0cff436a47
Martin <martinjonsson01@gmail.com>
2025-03-09 15:28:46 +0100

    480 current  2025-03-09 15:28:38  24.11.20250219.36864ed  6.6.78                          *

--- nixos/modules/home-manager/desktops/add-ons/gnome-screenshot/screenshot.sh
+++ nixos/modules/home-manager/desktops/add-ons/gnome-screenshot/screenshot.sh
@@ -3,4 +3,8 @@
 YEAR=$(date +%Y) || exit
 MONTH=$(date +%b) || exit
 TIME=$(date +'%H:%M:%S') || exit
+
+mkdir -p $HOME/Pictures/Screenshots/$YEAR
+mkdir -p $HOME/Pictures/Screenshots/$YEAR/$MONTH
+
 gnome-screenshot --file="$HOME/Pictures/Screenshots/$YEAR/$MONTH/$TIME.png" --clipboard "$@"
