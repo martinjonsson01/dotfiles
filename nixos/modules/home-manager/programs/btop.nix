3b3c7e4da9c018d0d7889552b1620edd15e58dcc
Martin <martinjonsson01@gmail.com>
2025-03-23 09:34:06 +0100

    514 current  2025-03-23 09:34:02  24.11.20250219.36864ed  6.6.78                          *

--- nixos/modules/home-manager/programs/btop.nix
+++ nixos/modules/home-manager/programs/btop.nix
@@ -2,6 +2,7 @@
 # A monitor of resources (a fancy version of `top`)
 #
 {
+  pkgs,
   lib,
   config,
   ...
@@ -13,8 +14,12 @@
   config = lib.mkIf config.btop.enable {
     programs.btop = {
       enable = true;
+      # Nvidia GPU support
+      # https://github.com/aristocratos/btop/issues/426#issuecomment-2103598718
+      package = pkgs.btop.override {cudaSupport = true;};
+
       settings = {
-        color_theme = "Tokyo Night";
+        color_theme = "tokyo-night";
         shown_boxes = "proc cpu mem"; # Manually set which boxes to show. Available values are "cpu mem net proc" and "gpu0" through "gpu5", separate values with whitespace.
         update_ms = 500; # Update time in milliseconds, recommended 2000 ms or above for better sample times for graphs.
         proc_per_core = true; # If process cpu usage should be of the core it's running on or usage of the total available cpu power.
