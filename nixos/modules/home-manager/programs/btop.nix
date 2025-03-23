904ee67b10bac753b7b4a6a16430f60171e74a9e
Martin <martinjonsson01@gmail.com>
2025-03-23 09:28:31 +0100

    512 current  2025-03-23 09:28:28  24.11.20250219.36864ed  6.6.78                          *

--- nixos/modules/home-manager/programs/btop.nix
+++ nixos/modules/home-manager/programs/btop.nix
@@ -15,7 +15,7 @@
       enable = true;
       settings = {
         shown_boxes = "proc cpu mem"; # Manually set which boxes to show. Available values are "cpu mem net proc" and "gpu0" through "gpu5", separate values with whitespace.
-        update_ms = 200; # Update time in milliseconds, recommended 2000 ms or above for better sample times for graphs.
+        update_ms = 500; # Update time in milliseconds, recommended 2000 ms or above for better sample times for graphs.
         proc_per_core = true; # If process cpu usage should be of the core it's running on or usage of the total available cpu power.
         cpu_single_graph = true; # Set to True to completely disable the lower CPU graph.
       };
