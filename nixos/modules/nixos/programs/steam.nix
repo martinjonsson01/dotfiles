a04625925e24bd359e5a965600df79141ece4b50
Martin <martinjonsson01@gmail.com>
2025-04-27 17:00:59 +0200

    536 current  2025-04-27 17:00:57  24.11.20250412.26d499f  6.6.87                          *

--- nixos/modules/nixos/programs/steam.nix
+++ nixos/modules/nixos/programs/steam.nix
@@ -14,6 +14,8 @@
   config = lib.mkIf config.steam.enable {
     environment.systemPackages = with pkgs; [
       steam
+      # The FHS-compatible chroot used for Steam can also be used to run other Linux games that expect a FHS environment.
+      steam-run
     ];
 
     # We need 32bit versions of all the OpenGL etc libraries for steam to run
