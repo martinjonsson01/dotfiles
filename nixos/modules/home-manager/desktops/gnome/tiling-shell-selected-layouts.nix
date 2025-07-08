0e490f47c4749cdfe5e9f2b8603df146b8614d66
Martin <martinjonsson01@gmail.com>
2025-07-08 20:57:22 +0200

    561 current  2025-07-08 20:57:20  25.05.20250525.7c43f08  6.12.30                          *

--- nixos/modules/home-manager/desktops/gnome/tiling-shell-selected-layouts.nix
+++ nixos/modules/home-manager/desktops/gnome/tiling-shell-selected-layouts.nix
@@ -1,27 +1,27 @@
 [
   [
+    "Big Center Layout"
     "Two Horizontal Stack Layout"
     "Three Vertical Stack Layout"
-    "Big Center Layout"
   ]
   [
+    "Big Center Layout"
     "Two Horizontal Stack Layout"
     "Three Vertical Stack Layout"
-    "Big Center Layout"
   ]
   [
+    "Big Center Layout"
     "Two Horizontal Stack Layout"
     "Three Vertical Stack Layout"
-    "Big Center Layout"
   ]
   [
+    "Big Center Layout"
     "Two Horizontal Stack Layout"
     "Three Vertical Stack Layout"
-    "Big Center Layout"
   ]
   [
+    "Big Center Layout"
     "Two Horizontal Stack Layout"
     "Three Vertical Stack Layout"
-    "Big Center Layout"
   ]
 ]
