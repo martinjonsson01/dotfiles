bc2578948e545761ad46578e90c5fc8a8459c171
Martin <martinjonsson01@gmail.com>
2025-07-10 08:31:57 +0200

    573 current  2025-07-10 08:31:54  25.05.20250708.88983d4  6.12.36                          *

--- nixos/modules/nixos/system/stylix.nix
+++ nixos/modules/nixos/system/stylix.nix
@@ -13,7 +13,7 @@
     stylix = {
       enable = true;
 
-      base16Scheme = "${pkgs.base16-schemes}/share/themes/gruvbox-dark-medium.yaml";
+      base16Scheme = "${pkgs.base16-schemes}/share/themes/tokyo-night-dark.yaml";
 
       cursor = {
         package = pkgs.bibata-cursors;
