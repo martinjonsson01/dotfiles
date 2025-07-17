261ee4b6e2cfca89213df8c0065acb8199e74bd5
Martin <martinjonsson01@gmail.com>
2025-07-17 12:44:36 +0200

    597 current  2025-07-17 12:44:34  25.05.20250708.88983d4  6.12.36                          *

--- nixos/modules/home-manager/desktops/niri/config/window-rules.nix
+++ nixos/modules/home-manager/desktops/niri/config/window-rules.nix
@@ -1,4 +1,17 @@
 {
   window-rules = [
+    # Add border radius to windows.
+    {
+      draw-border-with-background = false;
+      geometry-corner-radius = let
+        r = 8.0;
+      in {
+        top-left = r;
+        top-right = r;
+        bottom-left = r;
+        bottom-right = r;
+      };
+      clip-to-geometry = true;
+    }
   ];
 }
