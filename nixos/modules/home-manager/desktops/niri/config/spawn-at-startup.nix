589f2e50b7757f638a1f6b24a70a5b4887df4f30
Martin <martinjonsson01@gmail.com>
2025-07-17 11:37:37 +0200

    588 current  2025-07-17 11:37:35  25.05.20250708.88983d4  6.12.36                          *

--- nixos/modules/home-manager/desktops/niri/config/spawn-at-startup.nix
+++ nixos/modules/home-manager/desktops/niri/config/spawn-at-startup.nix
@@ -7,12 +7,12 @@
       # To support X11 applications
       {command = ["${pkgs.xwayland-satellite}/bin/xwayland-satellite"];}
       # Waybar status bar
-      {command = ["pidof ${pkgs.waybar}/bin/waybar || ${pkgs.waybar}/bin/waybar"];}
+      {command = ["pidof ${pkgs.waybar}/bin/waybar"];}
     ]
     # Wallpapers
     ++ builtins.map (
       monitor: {
-        command = ["${pkgs.swaybg}/bin/swaybg -o ${monitor.connector} -i ${monitor.wallpaper}"];
+        command = ["${pkgs.swaybg}/bin/swaybg" "-o" "${monitor.connector}" "-i" "${monitor.wallpaper}"];
       }
     ) (
       builtins.filter (m: m.wallpaper != null) myHardware.monitors
