ac7497dd992e0aa873fb20ab1b3261dfe62c0452
Martin <martinjonsson01@gmail.com>
2025-07-17 17:17:24 +0200

    660 current  2025-07-17 17:17:20  25.05.20250708.88983d4  6.12.36                          *

--- nixos/modules/home-manager/desktops/niri/config/window-rules.nix
+++ nixos/modules/home-manager/desktops/niri/config/window-rules.nix
@@ -15,6 +15,17 @@
       };
       clip-to-geometry = true;
     }
+    # Open browser windows in small width.
+    {
+      matches = [
+        {
+          app-id = "^google-chrome$";
+        }
+      ];
+      open-floating = false;
+      default-column-width = {proportion = 2.0 / 7.0;};
+      default-window-height = {proportion = 1.0;};
+    }
     # Open some apps in wide mode.
     {
       matches = [
