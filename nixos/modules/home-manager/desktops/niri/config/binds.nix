e587fe8d790ad05d20d3731198b071e50e1b9178
Martin <martinjonsson01@gmail.com>
2025-07-18 14:57:55 +0200

    707 current  2025-07-18 14:57:51  25.05.20250708.88983d4  6.12.36                          *

--- nixos/modules/home-manager/desktops/niri/config/binds.nix
+++ nixos/modules/home-manager/desktops/niri/config/binds.nix
@@ -200,6 +200,7 @@ with lib; {
     # "Mod+At".action = switch-layout "next";
     # Mod+Shift+Space { switch-layout "prev"; }
 
+    "Ctrl+Print".action = spawn ["screenshot-region.sh"];
     "Ctrl+Shift+Print".action = spawn ["screenrecord-region.sh"];
     "Print".action = screenshot;
     "Alt+Print".action = screenshot-window;
