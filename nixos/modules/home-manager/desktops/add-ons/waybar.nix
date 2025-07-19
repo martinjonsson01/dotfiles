ece8f5432be1ca5fa3bb64f5c7fd5869b330ebac
Martin <martinjonsson01@gmail.com>
2025-07-19 14:35:55 +0200

    789 current  2025-07-19 14:35:49  25.05.20250708.88983d4  6.12.36                          *

--- nixos/modules/home-manager/desktops/add-ons/waybar.nix
+++ nixos/modules/home-manager/desktops/add-ons/waybar.nix
@@ -138,7 +138,7 @@ with lib; let
     "custom/nvidia" = {
       exec = "nvidia-smi --query-gpu=utilization.gpu,temperature.gpu --format=csv,nounits,noheader | sed 's/\\([0-9]\\+\\), \\([0-9]\\+\\)/\\1% \\2°C/g'";
       format = "🖥️ {}";
-      interval = 1;
+      interval = 30;
     };
 
     # Display battery statuses.
