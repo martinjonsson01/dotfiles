d5804fde68debd68db68688eb62e1ce841e7ee1c
Martin <martinjonsson01@gmail.com>
2025-07-19 11:23:45 +0200

    749 current  2025-07-19 11:23:40  25.05.20250708.88983d4  6.12.36                          *

--- nixos/modules/home-manager/desktops/niri/config/binds.nix
+++ nixos/modules/home-manager/desktops/niri/config/binds.nix
@@ -27,7 +27,7 @@ with lib; {
     "Mod+L".action = spawn ["systemctl" "suspend"];
     "Mod+S".action = spawn "${getExe pkgs.fsearch}";
     "Mod+Tab".action = spawn ["${getExe pkgs.rofi}" "-show" "window"];
-    "Mod+E".action = spawn ["${getExe pkgs.nemo}"];
+    "Mod+E".action = spawn ["${getExe pkgs.nemo}" "/home/martin/Downloads"];
     "Mod+P".action = spawn ["open-screenshot-dir.sh"];
     "Mod+Space".action = spawn "${getExe pkgs.qalculate-gtk}";
 
