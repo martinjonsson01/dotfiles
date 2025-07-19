bd7693fa13fb020febf121174575dae1f61be5d1
Martin <martinjonsson01@gmail.com>
2025-07-19 12:45:41 +0200

    757 current  2025-07-19 12:45:37  25.05.20250708.88983d4  6.12.36                          *

--- nixos/secrets/kyltermometer.sh
+++ nixos/secrets/kyltermometer.sh
@@ -1,5 +1,5 @@
 {
-	"data": "ENC[AES256_GCM,data:16x3pBAn0wmooz+onJyNsMf6MrVVFnLBaVtTfbLRjIZUaDyVNhzdJqHETrrMvH5vQMdcK/MVzDlU9YXuMF/zpwMrMUgYff41vbPSSuNLTIVvZfmDO/Y3vpTJBLyH0K3vP+psg5k2trgz5u90zdAQTYa8LB9r/CtkgpTjOWhfHtHlx9iwU49cnEfrQPHNKtpzVVyA9gT+X3z6NkYnBY2YSTS2,iv:boKKzOjuC6rQjinRWFmjPcpoCtytIY0U1WIvzhsQVVA=,tag:81x1FKa3d7SgEZHztrFHcg==,type:str]",
+	"data": "ENC[AES256_GCM,data:7mqx/okARm1lMkgPYurku/oe6SHyWe9m/0Ev3qo4PibfmQKDxqQTCe6KAfCuAZ+hm4zPbskmC5pHe6Vg71EAnc20s0670ZzyZSHEcWzft+Dw5oIfRhOtiY4ADbHxgvVhqD7DqolQIIGexBJh4aSfrLtiw2gqIY0gfsvD+UgUpWYG0KfMkWOmO0xUCeRLzjBrQX6kXg97h/WUHyrpNcEhcCoYAgLVkpAvO7wNf7CAtWQiGJyRbvaLtC7++evpvtgn05J6rd+tcgeZ,iv:hCitihRmUGCj4ppE7hsCGjid4hhTcZs/cS9TDDqWKOk=,tag:j1yVBQE7R7+ZsTEyatrObA==,type:str]",
 	"sops": {
 		"age": [
 			{
@@ -7,8 +7,8 @@
 				"enc": "-----BEGIN AGE ENCRYPTED FILE-----\nYWdlLWVuY3J5cHRpb24ub3JnL3YxCi0+IFgyNTUxOSBTQ3N6eCsrYkRKRW9md3kz\nVUpxNjBpaDY2elNvZTQxSWI4dzJoUjUrTld3CnFFQ2hseU1POTVmSHFoNHN2dXV4\nUnpKaDJob2loRmdCNFlFTi9HY0F3MlEKLS0tIHpFRGRsM2dsWTR5ZFp5OThrM2tY\nL3ovR0kwd3VZdFJRV0VTdmFVdzhwbFEK5pbwR2l4L0DI9wDLBlxBfwEIY2BuBj4s\n4jIMIod8HbDe8zNSzWSggHfXTvxQnRmNTmb/6eSerixrKPiEPhAmZg==\n-----END AGE ENCRYPTED FILE-----\n"
 			}
 		],
-		"lastmodified": "2025-07-19T10:34:49Z",
-		"mac": "ENC[AES256_GCM,data:WnxZBFHqmwRujMEXle36DKp68ZyM1+TXQfb6PFMQnC/h/nOOax82j7t8RZ6S+/tobnDBMvvm6pYMvzhvZAOiXvNORGp9IB1hBkUgyh+TY1IJ92DY+QIWxI5um7v6nVw5sHosIwbw1eh0TAD2JILe4l2d56CBNz2CIf4Y/utKW3Y=,iv:3Ap0St1K0VdA0B4s5vEb/EwCuvSSilxiOMs+DZDhMYA=,tag:Z20kjnMBUAqlSf8JfxgtLA==,type:str]",
+		"lastmodified": "2025-07-19T10:45:12Z",
+		"mac": "ENC[AES256_GCM,data:iEuBNjblwSEDDk+i+IoLR2k1wX2w8PFOCzWS3uewGYODPq5Kh1+9wkeBlJXawrfvCswmBIP7Lin96TLugY2kVm8WdWEKXk/DAQKLaPDBlJwYsdi17N1m8E4OkNgnTQe5PqDcZMr23IYdjdsyAUs1xZmhzkeUt+OrqKifc1uzbMw=,iv:j5khgWvKx+gMhw6XCbAIhxjdnZfnFwaTreKMEZosWvU=,tag:XjCxict3IJGKehUEGwatGA==,type:str]",
 		"unencrypted_suffix": "_unencrypted",
 		"version": "3.10.2"
 	}
