5413f76196856a2045bf0f3018f72d12c7f47340
Martin <martinjonsson01@gmail.com>
2025-07-20 09:34:43 +0200

    822 current  2025-07-20 09:34:36  25.05.20250708.88983d4  6.12.36                          *

--- nixos/modules/home-manager/desktops/add-ons/gammastep.nix
+++ nixos/modules/home-manager/desktops/add-ons/gammastep.nix
@@ -8,22 +8,18 @@
   pkgs,
   lib,
   ...
-}: let
-  startTime = "19:00:00";
-  endTime = "05:00:00";
-  temperature = 3000;
-in
-  with lib; {
-    options = {
-      gammastep.enable = lib.mkEnableOption "Enables gammastep";
-    };
+}:
+with lib; {
+  options = {
+    gammastep.enable = lib.mkEnableOption "Enables gammastep";
+  };
 
-    config = lib.mkIf config.gammastep.enable {
-      services.gammastep = {
-        enable = true;
-        temperature.night = 3000;
-        dawnTime = "05:00";
-        duskTime = "19:00";
-      };
+  config = lib.mkIf config.gammastep.enable {
+    services.gammastep = {
+      enable = true;
+      temperature.night = 3000;
+      dawnTime = "09:36";
+      duskTime = "09:35";
     };
-  }
+  };
+}
