77d20e46ee575ce5c91581c5080c75c48967e2ee
Martin <martinjonsson01@gmail.com>
2025-07-26 11:34:13 +0200

    863 current  2025-07-26 11:34:07  25.05.20250724.3ff0e34  6.12.39                          *

--- nixos/modules/home-manager/desktops/default-applications.nix
+++ nixos/modules/home-manager/desktops/default-applications.nix
@@ -11,4 +11,11 @@
     "x-scheme-handler/mailto" = "google-chrome.desktop";
     "inode/directory" = "thunar.desktop";
   };
+
+  # Override which file manager is used by dbus.
+  xdg.dataFile."dbus-1/services/org.freedesktop.FileManager1.service".text = ''
+    [D-BUS Service]
+    Name=org.freedesktop.FileManager1
+    Exec=thunar
+  '';
 }
