ec90a3c04e8a76ae5598454ebf88bcab8ee2fa41
Martin <martinjonsson01@gmail.com>
2025-07-27 15:58:25 +0200

    894 current  2025-07-27 15:58:18  25.05.20250724.3ff0e34  6.12.39                          *

--- nixos/modules/home-manager/desktops/niri/config/window-rules.nix
+++ nixos/modules/home-manager/desktops/niri/config/window-rules.nix
@@ -36,6 +36,7 @@
         {app-id = "^fsearch$";}
         {app-id = "^org.gnome.NautilusPreviewer$";}
         {app-id = "^org.gnome.Loupe$";}
+        {app-id = "^vlc$";}
         {
           app-id = "^jetbrains-rustrover$";
           title = "^ $";
