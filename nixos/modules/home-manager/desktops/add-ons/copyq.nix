1b932a47e15ac680e961062d2d5354b0ce06b853
Martin <martinjonsson01@gmail.com>
2025-07-30 13:14:08 +0200

    929 current  2025-07-30 13:14:01  25.05.20250724.3ff0e34  6.12.39                          *

--- nixos/modules/home-manager/desktops/add-ons/copyq.nix
+++ nixos/modules/home-manager/desktops/add-ons/copyq.nix
@@ -51,6 +51,8 @@ with lib; {
           }
         ];
         open-floating = true;
+        default-column-width = {fixed = 300;};
+        default-window-height = {fixed = 800;};
       }
     ];
 
