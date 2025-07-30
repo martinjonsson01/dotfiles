7c6950b7ce42eadb7469edcd7a70c50c4bacca8c
Martin <martinjonsson01@gmail.com>
2025-07-30 10:40:47 +0200

    911 current  2025-07-30 10:40:40  25.05.20250724.3ff0e34  6.12.39                          *

--- nixos/modules/home-manager/desktops/default-applications.nix
+++ nixos/modules/home-manager/desktops/default-applications.nix
@@ -2,7 +2,7 @@
   xdg.mime.defaultApplications = {
     "text/*" = "org.gnome.TextEditor.desktop";
     "image/*" = "org.gnome.Loupe.desktop";
-    "video/*" = "org.gnome.Totem.desktop";
+    "video/*" = "mpv.desktop";
     "audio/*" = "org.gnome.Decibels.desktop";
     "application/pdf" = "google-chrome.desktop";
     "x-scheme-handler/http" = "google-chrome.desktop";
