0646ab1a6b7976026c707040a805eecdb0d02487
Martin <martinjonsson01@gmail.com>
2025-07-30 10:05:54 +0200

    905 current  2025-07-30 10:05:46  25.05.20250724.3ff0e34  6.12.39                          *

--- nixos/modules/home-manager/desktops/add-ons/gtk-theme.nix
+++ nixos/modules/home-manager/desktops/add-ons/gtk-theme.nix
@@ -36,7 +36,7 @@ with lib; {
       };
       iconTheme = mkForce {
         name = "Tokyonight-Dark";
-        package = pkgs.morewaita-icon-theme;
+        package = pkgs.tokyonight-gtk-theme.override {iconVariants = ["Dark"];};
       };
       gtk3.extraConfig = {
         gtk-application-prefer-dark-theme = 1;
