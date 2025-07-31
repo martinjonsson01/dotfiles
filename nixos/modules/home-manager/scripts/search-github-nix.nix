45132368f9321837dbe2f04d8ed8768419d79748
Martin <martinjonsson01@gmail.com>
2025-07-31 10:07:36 +0200

    955 current  2025-07-31 10:07:28  25.05.20250724.3ff0e34  6.12.39                          *

--- nixos/modules/home-manager/scripts/search-github-nix.nix
+++ nixos/modules/home-manager/scripts/search-github-nix.nix
@@ -9,8 +9,6 @@
 }:
 with lib; let
   search-github-nix = pkgs.writers.writeBashBin "search-github-nix.sh" ''
-    #!/usr/bin/env bash
-
     set -o errexit   # abort on nonzero exitstatus
     set -o nounset   # abort on unbound variable
     set -o pipefail  # don't hide errors within pipes
--- nixos/modules/home-manager/scripts/search-google.nix
+++ nixos/modules/home-manager/scripts/search-google.nix
@@ -9,8 +9,6 @@
 }:
 with lib; let
   search-google = pkgs.writers.writeBashBin "search-google.sh" ''
-    #!/usr/bin/env bash
-
     set -o errexit   # abort on nonzero exitstatus
     set -o nounset   # abort on unbound variable
     set -o pipefail  # don't hide errors within pipes
