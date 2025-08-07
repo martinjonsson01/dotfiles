760fe3609733dfb5f8563a8474bc1b2ed0bfc451
Martin <martinjonsson01@gmail.com>
2025-08-07 14:31:30 +0200

    1017 current  2025-08-07 14:31:25  25.05.20250724.3ff0e34  6.12.39                          *

--- nixos/modules/home-manager/programs/lutris.nix
+++ nixos/modules/home-manager/programs/lutris.nix
@@ -17,11 +17,12 @@ with lib; {
       enable = true;
       protonPackages = [pkgs.proton-ge-bin];
       extraPackages = with pkgs; [
-        mangohud
-        winetricks
-        gamescope
-        gamemode
-        umu-launcher
+        mangohud # Vulkan and OpenGL overlay for monitoring FPS, temperatures, CPU/GPU load and more
+        winetricks # Script to install DLLs needed to work around problems in Wine
+        gamescope # SteamOS session compositing window manager
+        gamemode # Optimise Linux system performance on demand
+        umu-launcher # Unified launcher for Windows games on Linux using the Steam Linux Runtime and Tools
+        bottles # Easily run Windows software on Linux with Bottles! (wine)
       ];
     };
   };
