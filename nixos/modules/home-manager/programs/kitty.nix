4f3cf5cbef33d78898e38ce6bdce6c0603f9f23f
Martin <martinjonsson01@gmail.com>
2025-08-10 17:20:01 +0200

    Adjust terminal look for nvim

--- nixos/modules/home-manager/programs/kitty.nix
+++ nixos/modules/home-manager/programs/kitty.nix
@@ -30,9 +30,10 @@ with lib; {
         tab_separator = "|";
         tab_title_template = "{fmt.fg.red}{bell_symbol}{activity_symbol}{fmt.fg.tab}{index}:{title}";
         confirm_os_window_close = 0;
-        window_padding_width = "8.0";
-        background_opacity = mkForce 0.95;
+        background_opacity = mkForce 1.0;
         background_blur = 1;
+        window_padding_width = "0";
+        window_padding_height = "0";
       };
 
       keybindings = {
