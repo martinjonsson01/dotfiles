6ba763f29eae957a9c0d1451fbacaf50a50a8855
Martin <martinjonsson01@gmail.com>
2025-08-12 16:02:23 +0200

    Format injected languages in nix files

--- nixos/modules/home-manager/editors/neovim/config/plugins/lsp/conform.nix
+++ nixos/modules/home-manager/editors/neovim/config/plugins/lsp/conform.nix
@@ -10,33 +10,33 @@
         local slow_format_filetypes = {}
 
         vim.api.nvim_create_user_command("FormatDisable", function(args)
-           if args.bang then
-            -- FormatDisable! will disable formatting just for this buffer
-            vim.b.disable_autoformat = true
-          else
-            vim.g.disable_autoformat = true
-          end
+        	if args.bang then
+        		-- FormatDisable! will disable formatting just for this buffer
+        		vim.b.disable_autoformat = true
+        	else
+        		vim.g.disable_autoformat = true
+        	end
         end, {
-          desc = "Disable autoformat-on-save",
-          bang = true,
+        	desc = "Disable autoformat-on-save",
+        	bang = true,
         })
         vim.api.nvim_create_user_command("FormatEnable", function()
-          vim.b.disable_autoformat = false
-          vim.g.disable_autoformat = false
+        	vim.b.disable_autoformat = false
+        	vim.g.disable_autoformat = false
         end, {
-          desc = "Re-enable autoformat-on-save",
+        	desc = "Re-enable autoformat-on-save",
         })
         vim.api.nvim_create_user_command("FormatToggle", function(args)
-          if args.bang then
-            -- Toggle formatting for current buffer
-            vim.b.disable_autoformat = not vim.b.disable_autoformat
-          else
-            -- Toggle formatting globally
-            vim.g.disable_autoformat = not vim.g.disable_autoformat
-          end
+        	if args.bang then
+        		-- Toggle formatting for current buffer
+        		vim.b.disable_autoformat = not vim.b.disable_autoformat
+        	else
+        		-- Toggle formatting globally
+        		vim.g.disable_autoformat = not vim.g.disable_autoformat
+        	end
         end, {
-          desc = "Toggle autoformat-on-save",
-          bang = true,
+        	desc = "Toggle autoformat-on-save",
+        	bang = true,
         })
       '';
     plugins.conform-nvim = {
@@ -106,7 +106,7 @@
             "isort"
           ];
           lua = ["stylua"];
-          nix = ["alejandra"];
+          nix = ["alejandra" "injected"];
           markdown = {
             __unkeyed-1 = "prettierd";
             __unkeyed-2 = "prettier";
@@ -165,6 +165,11 @@
           #yamlfmt = {
           #  command = "${lib.getExe pkgs.yamlfmt}";
           #};
+          injected = {
+            options = {
+              ignore_errors = true; # Partial code snippets can break formatters.
+            };
+          };
         };
       };
     };
