46be6b928640881f47572f2083905695755a1e6b
Martin <martinjonsson01@gmail.com>
2025-08-20 09:37:52 +0200

    Flatten attributes

--- nixos/modules/home-manager/editors/neovim/config/plugins/lsp/lsp.nix
+++ nixos/modules/home-manager/editors/neovim/config/plugins/lsp/lsp.nix
@@ -1,14 +1,8 @@
 {pkgs, ...}: {
   plugins = {
-    lsp-lines = {
-      enable = true;
-    };
-    lsp-format = {
-      enable = true;
-    };
-    helm = {
-      enable = true;
-    };
+    lsp-lines.enable = true;
+    lsp-format.enable = true;
+    helm.enable = true;
     lsp = {
       enable = true;
       inlayHints = true;
@@ -144,24 +138,20 @@
   extraConfigLua = ''
     local _border = "rounded"
 
-    vim.lsp.handlers["textDocument/hover"] = vim.lsp.with(
-      vim.lsp.handlers.hover, {
-        border = _border
-      }
-    )
+    vim.lsp.handlers["textDocument/hover"] = vim.lsp.with(vim.lsp.handlers.hover, {
+    	border = _border,
+    })
 
-    vim.lsp.handlers["textDocument/signatureHelp"] = vim.lsp.with(
-      vim.lsp.handlers.signature_help, {
-        border = _border
-      }
-    )
+    vim.lsp.handlers["textDocument/signatureHelp"] = vim.lsp.with(vim.lsp.handlers.signature_help, {
+    	border = _border,
+    })
 
-    vim.diagnostic.config{
-      float={border=_border}
-    };
+    vim.diagnostic.config({
+    	float = { border = _border },
+    })
 
-    require('lspconfig.ui.windows').default_options = {
-      border = _border
+    require("lspconfig.ui.windows").default_options = {
+    	border = _border,
     }
   '';
 }
