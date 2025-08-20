d8bff7a4ec804a0b73fa5e77abc4decc5f6ea1e4
Martin <martinjonsson01@gmail.com>
2025-08-20 11:19:39 +0200

    Flatten attributes

--- nixos/modules/home-manager/editors/neovim/config/plugins/lsp/conform.nix
+++ nixos/modules/home-manager/editors/neovim/config/plugins/lsp/conform.nix
@@ -135,42 +135,17 @@
         };
 
         formatters = {
-          black = {
-            command = "${lib.getExe pkgs.black}";
-          };
-          isort = {
-            command = "${lib.getExe pkgs.isort}";
-          };
-          nixfmt-rfc-style = {
-            command = "${lib.getExe pkgs.nixfmt-rfc-style}";
-          };
-          alejandra = {
-            command = "${lib.getExe pkgs.alejandra}";
-          };
-          jq = {
-            command = "${lib.getExe pkgs.jq}";
-          };
-          prettierd = {
-            command = "${lib.getExe pkgs.prettierd}";
-          };
-          stylua = {
-            command = "${lib.getExe pkgs.stylua}";
-          };
-          shellcheck = {
-            command = "${lib.getExe pkgs.shellcheck}";
-          };
-          shfmt = {
-            command = "${lib.getExe pkgs.shfmt}";
-          };
-          shellharden = {
-            command = "${lib.getExe pkgs.shellharden}";
-          };
-          bicep = {
-            command = "${lib.getExe pkgs.bicep}";
-          };
-          #yamlfmt = {
-          #  command = "${lib.getExe pkgs.yamlfmt}";
-          #};
+          black.command = "${lib.getExe pkgs.black}";
+          isort.command = "${lib.getExe pkgs.isort}";
+          nixfmt-rfc-style.command = "${lib.getExe pkgs.nixfmt-rfc-style}";
+          alejandra.command = "${lib.getExe pkgs.alejandra}";
+          jq.command = "${lib.getExe pkgs.jq}";
+          prettierd.command = "${lib.getExe pkgs.prettierd}";
+          stylua.command = "${lib.getExe pkgs.stylua}";
+          shellcheck.command = "${lib.getExe pkgs.shellcheck}";
+          shfmt.command = "${lib.getExe pkgs.shfmt}";
+          shellharden.command = "${lib.getExe pkgs.shellharden}";
+          bicep.command = "${lib.getExe pkgs.bicep}";
           injected = {
             options = {
               ignore_errors = true; # Partial code snippets can break formatters.
