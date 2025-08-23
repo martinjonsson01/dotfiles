8fb6a8843bb21e63baca7e233c4c113d6ead8880
Martin <martinjonsson01@gmail.com>
2025-08-23 14:17:53 +0200

    Add snippets

--- nixos/modules/home-manager/editors/neovim/config/plugins/snippets/luasnip.nix
+++ nixos/modules/home-manager/editors/neovim/config/plugins/snippets/luasnip.nix
@@ -1,9 +1,19 @@
-_: {
-  plugins.luasnip = {
-    enable = true;
-    settings = {
-      enable_autosnippets = true;
-      store_selection_keys = "<Tab>";
+{pkgs, ...}: {
+  plugins = {
+    friendly-snippets.enable = true;
+
+    luasnip = {
+      enable = true;
+      settings = {
+        enable_autosnippets = true;
+        store_selection_keys = "<Tab>";
+      };
+      fromVscode = [
+        {
+          lazyLoad = true;
+          paths = "${pkgs.vimPlugins.friendly-snippets}";
+        }
+      ];
     };
   };
 }
