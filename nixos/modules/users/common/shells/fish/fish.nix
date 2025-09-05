54827578668f02b5eff7ceb824b6864a023b5698
Martin <martinjonsson01@gmail.com>
2025-09-05 06:45:27 +0200

    Add fish helpers

--- nixos/modules/users/common/shells/fish/fish.nix
+++ nixos/modules/users/common/shells/fish/fish.nix
@@ -102,26 +102,32 @@ with lib; {
         }
       ];
 
-      interactiveShellInit = ''
-        ${pkgs.any-nix-shell}/bin/any-nix-shell fish --info-right | source
-        ${pkgs.thefuck}/bin/thefuck --alias | source
+      interactiveShellInit =
+        #fish
+        ''
+          ${pkgs.any-nix-shell}/bin/any-nix-shell fish --info-right | source
+          ${pkgs.thefuck}/bin/thefuck --alias | source
 
-        set -gx FZF_DEFAULT_COMMAND 'fd --type f --hidden --exclude .git'
-        set -gx FZF_CTRL_T_COMMAND $FZF_DEFAULT_COMMAND
-        set -gx FZF_ALT_C_COMMAND 'fd --type d .'
+          if test -e ${config.xdg.configHome}/fish/functions/fish_helpers.fish
+            source ${config.xdg.configHome}/fish/functions/fish_helpers.fish
+          end
 
-        # Set Fish colors that aren't dependant the `$term_background`.
-        set -g fish_color_quote        cyan      # color of commands
-        set -g fish_color_redirection  brmagenta # color of IO redirections
-        set -g fish_color_end          blue      # color of process separators like ';' and '&'
-        set -g fish_color_error        red       # color of potential errors
-        set -g fish_color_match        --reverse # color of highlighted matching parenthesis
-        set -g fish_color_search_match --background=yellow
-        set -g fish_color_selection    --reverse # color of selected text (vi mode)
-        set -g fish_color_operator     green     # color of parameter expansion operators like '*' and '~'
-        set -g fish_color_escape       red       # color of character escapes like '\n' and and '\x70'
-        set -g fish_color_cancel       red       # color of the '^C' indicator on a canceled command
-      '';
+          set -gx FZF_DEFAULT_COMMAND 'fd --type f --hidden --exclude .git'
+          set -gx FZF_CTRL_T_COMMAND $FZF_DEFAULT_COMMAND
+          set -gx FZF_ALT_C_COMMAND 'fd --type d .'
+
+          # Set Fish colors that aren't dependant the `$term_background`.
+          set -g fish_color_quote        cyan      # color of commands
+          set -g fish_color_redirection  brmagenta # color of IO redirections
+          set -g fish_color_end          blue      # color of process separators like ';' and '&'
+          set -g fish_color_error        red       # color of potential errors
+          set -g fish_color_match        --reverse # color of highlighted matching parenthesis
+          set -g fish_color_search_match --background=yellow
+          set -g fish_color_selection    --reverse # color of selected text (vi mode)
+          set -g fish_color_operator     green     # color of parameter expansion operators like '*' and '~'
+          set -g fish_color_escape       red       # color of character escapes like '\n' and and '\x70'
+          set -g fish_color_cancel       red       # color of the '^C' indicator on a canceled command
+        '';
     };
 
     # Greeting
