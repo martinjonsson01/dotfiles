06dc08dd7cf19274b727b5ccf2b7f3019fc976ca
Martin <martinjonsson01@gmail.com>
2025-09-11 07:55:33 +0200

    Customize gdbinit

--- nixos/modules/users/common/programs/gdb-dashboard.nix
+++ nixos/modules/users/common/programs/gdb-dashboard.nix
@@ -26,6 +26,39 @@ in {
       python313Packages.pygments
     ];
 
-    home.file.".gdbinit".source = gdb-dashboard-gdbinit;
+    home.file.".gdbinit".text = ''
+      source ${gdb-dashboard-gdbinit}
+
+      # Set dashboard style
+      define src
+        dashboard -layout expressions registers source variables stack
+        dashboard source -style height 20
+      end
+
+      define asm
+        dashboard -layout expressions assembly registers
+        dashboard assembly -style height 20
+      end
+
+      src
+
+      # Auto-accept pending breakpoints
+      set breakpoint pending on
+
+      # Store GDB command history in ~/.gdb_history
+      set history filename ~/.gdb_history
+
+      # Enable automatic saving of command history
+      set history save on
+
+      # Allow unlimited history entries
+      set history size unlimited
+
+      # Remove duplicate entries from history
+      set history remove-duplicates unlimited
+
+      # Enable history expansion (like !n to repeat nth command)
+      set history expansion on
+    '';
   };
 }
