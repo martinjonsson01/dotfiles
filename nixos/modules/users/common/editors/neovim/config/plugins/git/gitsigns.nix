f0e0c1250851acc99b62140e5189cf86f3b0ab3f
Martin <martinjonsson01@gmail.com>
2025-10-12 08:50:14 +0200

    Add gitsigns keybinds

--- nixos/modules/users/common/editors/neovim/config/plugins/git/gitsigns.nix
+++ nixos/modules/users/common/editors/neovim/config/plugins/git/gitsigns.nix
@@ -1,4 +1,9 @@
-_: {
+{
+  lib,
+  config,
+  ...
+}:
+with lib; {
   plugins.gitsigns = {
     enable = true;
     settings = {
@@ -25,4 +30,47 @@ _: {
       };
     };
   };
+
+  keymaps = mkIf config.plugins.gitsigns.enable [
+    {
+      mode = "n";
+      key = "<leader>gb";
+      action = "<cmd>Gitsigns blame_line { full=true }<CR>";
+      options = {
+        desc = "Git blame full line";
+      };
+    }
+    {
+      mode = "n";
+      key = "<leader>gtb";
+      action = "<cmd>Gitsigns blame<CR>";
+      options = {
+        desc = "Git toggle blame for entire file";
+      };
+    }
+    {
+      mode = "n";
+      key = "<leader>ghs";
+      action = "<cmd>Gitsigns stage_hunk<CR>";
+      options = {
+        desc = "Git hunk stage/unstage";
+      };
+    }
+    {
+      mode = "n";
+      key = "<leader>ghr";
+      action = "<cmd>Gitsigns reset_hunk<CR>";
+      options = {
+        desc = "Git hunk reset";
+      };
+    }
+    {
+      mode = "n";
+      key = "<leader>ghp";
+      action = "<cmd>Gitsigns preview_hunk_inline<CR>";
+      options = {
+        desc = "Git hunk preview inline";
+      };
+    }
+  ];
 }
