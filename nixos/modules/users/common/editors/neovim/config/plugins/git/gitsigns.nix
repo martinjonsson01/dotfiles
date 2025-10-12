8cd29e275e19b2a488476bb4463c2a1937c5ada3
Martin <martinjonsson01@gmail.com>
2025-10-12 09:01:42 +0200

    Remove ugly git symbols

--- nixos/modules/users/common/editors/neovim/config/plugins/git/gitsigns.nix
+++ nixos/modules/users/common/editors/neovim/config/plugins/git/gitsigns.nix
@@ -8,26 +8,6 @@ with lib; {
     enable = true;
     settings = {
       current_line_blame = true;
-      signs = {
-        add = {
-          text = " ";
-        };
-        change = {
-          text = " ";
-        };
-        delete = {
-          text = " ";
-        };
-        untracked = {
-          text = "";
-        };
-        topdelete = {
-          text = "󱂥 ";
-        };
-        changedelete = {
-          text = "󱂧 ";
-        };
-      };
     };
   };
 
