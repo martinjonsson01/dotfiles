46f2551dc97652abeee4af36ebab740311fc23c6
Martin <martinjonsson01@gmail.com>
2026-01-28 09:23:41 +0100

    Add mini.move

--- nixos/modules/users/common/editors/neovim/config/plugins/utils/mini.nix
+++ nixos/modules/users/common/editors/neovim/config/plugins/utils/mini.nix
@@ -5,6 +5,7 @@
     modules = {
       surround = {};
       bufremove = {}; # Keeps windows in place when closing buffer, replacing the buffer inside.
+      move = {}; # Move selection while preserving it and reindenting it
     };
   };
 }
