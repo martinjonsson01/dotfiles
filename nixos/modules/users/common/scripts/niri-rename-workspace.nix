b246ddc3e834a87e2bfccdcfe14994d38991e14a
Martin <martinjonsson01@gmail.com>
2026-01-30 08:15:31 +0100

    Add unset alternative to workspace rename

--- nixos/modules/users/common/scripts/niri-rename-workspace.nix
+++ nixos/modules/users/common/scripts/niri-rename-workspace.nix
@@ -22,9 +22,9 @@ with lib; let
       # If fuzzel is canceled, abort and do nothing
       # If fuzzel input is empty, unset workspace name
       # Else rename workspace with provided name
-      response=$(fuzzel --dmenu --prompt-only "Rename workspace to: ")
+      response=$(echo "Unset" | fuzzel --dmenu --prompt "Rename workspace to: " --lines=1)
       trimmed=$(awk '{$1=$1;print}' <<<"$response")
-      if [ "$trimmed" = "" ]; then
+      if [ "$trimmed" = "Unset" ]; then
         niri msg action unset-workspace-name
       else
         niri msg action set-workspace-name "$response"
