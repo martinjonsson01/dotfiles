0a2ca1b7fa1ec8a4084265ef45906759a854ba61
Martin <martinjonsson01@gmail.com>
2026-03-21 13:17:18 +0100

    Fix cursor restore not working

--- nixos/modules/users/common/editors/neovim/config/auto_cmds.nix
+++ nixos/modules/users/common/editors/neovim/config/auto_cmds.nix
@@ -30,6 +30,28 @@
       };
     }
     {
+      group = "vim_enter";
+      event = ["SessionLoadPost"];
+      pattern = "*";
+      callback = {
+        __raw = ''
+          function()
+            -- Snapshot correct position now, before float window creation
+            -- (fidget, incline, treesitter-context) displaces the cursor
+            local buf = vim.api.nvim_get_current_buf()
+            local pos = vim.api.nvim_win_get_cursor(0)
+            -- Defer so all plugin float initialization can settle first
+            vim.defer_fn(function()
+              -- Only restore if we haven't switched buffers in the meantime
+              if vim.api.nvim_get_current_buf() == buf then
+                vim.api.nvim_win_set_cursor(0, pos)
+              end
+            end, 100)
+          end
+        '';
+      };
+    }
+    {
       event = ["FileType"];
       pattern = "resc";
       command = "setlocal commentstring=#\\ %s";
