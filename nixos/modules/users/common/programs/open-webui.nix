39a884043fef89aa5a5a8b9e2b803cfc14389161
Martin <martinjonsson01@gmail.com>
2026-04-11 15:07:50 +0200

    Turn open-webui into service

--- nixos/modules/users/common/programs/open-webui.nix
+++ nixos/modules/users/common/programs/open-webui.nix
@@ -11,17 +11,31 @@ with lib; {
   options = {
     openWebui.enable = mkEnableOption "Enables Open WebUI";
   };
-
   config = mkIf config.openWebui.enable {
     home.packages = with pkgs; [
       unstable.open-webui
-      gvisor # For code execution sandboxing
     ];
 
-    home.sessionVariables = {
-      OLLAMA_BASE_URL = "http://localhost:11434";
-      WEBUI_AUTH = "False";
-      DATA_DIR = "${config.xdg.configHome}/open-webui";
+    systemd.user.services.open-webui = {
+      Unit = {
+        Description = "Open WebUI";
+        After = [
+          "network.target"
+          "ollama.service"
+        ];
+      };
+      Service = {
+        ExecStart = "${pkgs.unstable.open-webui}/bin/open-webui serve --port 38712";
+        Restart = "on-failure";
+        Environment = [
+          "OLLAMA_BASE_URL=http://localhost:11434"
+          "WEBUI_AUTH=False"
+          "DATA_DIR=${config.xdg.configHome}/open-webui"
+        ];
+      };
+      Install = {
+        WantedBy = ["default.target"];
+      };
     };
   };
 }
